5bd60a399ea6c710d612466155e18bb4d0019365
Dhanashri Adawade <adawadedhanashri@gmail.com>
2026-02-24 23:09:44 +0530

    Add files via upload

---
+++ 01_OOP_class.py
@@ -0,0 +1,11 @@
+# class as a blueprint or a template. example form it contains the name , age , date etc.
+
+#object is the instancs of the class.
+
+
+class Dog:
+    def bark(self):
+        print("Woof Woof!")
+
+d1 = Dog()
+d1.bark()
